912eb6fdbc37982001915374dc2869796c3f57b3
almo <thien.le@tum.de>
2023-01-22 15:26:49 +0100

    record and replayed sequence of arm movement, for testing

--- src/naoXophone/script/grabRecording.py
+++ src/naoXophone/script/grabRecording.py
@@ -4,8 +4,10 @@ import rospy
 import sys
 from sensor_msgs.msg import JointState
 import os 
+import qi
+import argparse
 from naoqi import ALProxy
-
+from naoqi_bridge_msgs.msg import HeadTouch
 
 # docs: http://doc.aldebaran.com/2-4/naoqi/motion/control-joint-api.html 
 
@@ -18,43 +20,58 @@ class grabSticks:
 
         # GETTING THE ANGLES
         # self.joint_sub = rospy.Subscriber('joint_states', JointState, self.joint_state_callback)
+        self.head_sub = rospy.Subscriber("/tactile_touch", HeadTouch, self.headtouch_callback)
         self.rightarm = ["RShoulderPitch","RShoulderRoll","RElbowYall","RElbowRoll","RWristYall","Rhand"]
+        self.leftarm = ["LShoulderPitch","LShoulderRoll","LElbowYall","LElbowRoll","LWristYall","Lhand"]
+        self.botharms = [*self.rightarm, *self.leftarm]
+        self.joint_sequence_start = motionProxy.getAngles(names=self.rightarm, useSensors=True)
+        print("Initial Position Recorded")
+        print("Place the hand on the joystick and press head button")
+        self.joint_sequence_end = []
+        self.headtouch = HeadTouch(0,0) # Init Headtouch message as empty
 
-        self.joint_squence = []
-        
 
-    # def joint_state_callback(self):
-# 
-        # pass
-
-    def recordJointState(self): 
-        pass
+    def headtouch_callback(self, headtouch):
+        self.headtouch = headtouch   
 
     def run(self): 
         # Position the Nao 
         # Let press the Head Button to start joint recording 
+        if self.headtouch.button is 1 and self.headtouch.state is 1:
+            self.joint_sequence_end = motionProxy.getAngles(names=self.botharms, useSensors=True)
+            print("Hand and joystick is recorded.")
         # Write these joint state into a file (So we could update this in the future-- Calibration)    
-        # Press again to stop 
 
         # Reset the state
         # Press another button to Repeat the sequence. 
-        pass
+        if self.headtouch.button is 2 and self.headtouch.state is 1: 
+            self.send_movement(self.joint_sequence_start)
+            self.send_movement(self.joint_sequence_end)
+
+        # self.rate.sleep()
 
 ##############
 # SEND MOVEMENT TO NAO 
 ##############
 
-    def send_movement(req):
-        print(req, type(req.joint_name), type(req.angle), type(req.speed))
+    def send_movement(self,position,stay_stiff=True):
+        self.motionProxy.setstiffnesses(self.botharms, [1.0 for i in self.botharms])
+        fractionMaxSpeed = 0.2
+        self.motionProxy.setAngles(self.botharms, position, fractionMaxSpeed)
 
-        if(req.relative):
-            print("Setting relative position to follow marker")
-            cur_angle = motionProxy.getAngles(req.joint_name, False)
-            req.angle = cur_angle[0]*almath.TO_DEG + req.angle
+        if not stay_stiff: 
+            self.motionProxy.setstiffnesses(self.botharms, [0.0 for i in self.botharms])
+
+        # print(req, type(req.joint_name), type(req.angle), type(req.speed))
+
+        # if(req.relative):
+        #     print("Setting relative position to follow marker")
+        #     cur_angle = motionProxy.getAngles(req.joint_name, False)
+        #     req.angle = cur_angle[0]*almath.TO_DEG + req.angle
         
-        req = check_limits(req)
-        motionProxy.setAngles(req.joint_name, req.angle*almath.TO_RAD, req.speed)
-        print(motionProxy.getTaskList())
+        # req = check_limits(req)
+        # motionProxy.setAngles(req.joint_name, req.angle*almath.TO_RAD, req.speed)
+        # print(motionProxy.getTaskList())
     #    time.sleep(3.0)
 
         #disable = rospy.ServiceProxy('body_stiffness/disable', Empty)
