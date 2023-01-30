f67a727e2ea549f0c5063c88df32919f213c1001
almolio <ge95kud@mytum.de>
2023-01-30 16:20:39 +0100

    play song

--- src/naoXophone/script/notePositions.py
+++ src/naoXophone/script/notePositions.py
@@ -39,26 +39,26 @@ class notePositions:
         L_Arm_joint_limits=self.motionProxy.getLimits("LArm")
         R_Arm_join_limits = self.motionProxy.getLimits("RArm")
         self.fractionMaxSpeed = 0.2
-        self.notePosition1 = [0.3589141368865967, 0.5302840347290039, -1.0078802108764648, -0.4662940502166748, 0.7853660583496094]
-        self.notePosition2 = [0.3589141368865967, 0.4432840347290039, -1.0078802108764648, -0.4662940502166748, 0.7853660583496094]
-        self.notePosition3 = [0.2709141368865967, 0.2682840347290039, -1.0078802108764648, -0.4662940502166748, 0.7853660583496094]
-        self.notePosition4 = [0.2709141368865967, 0.1832840347290039, -1.0078802108764648, -0.4662940502166748, 0.7853660583496094]
-        self.notePosition5 = [0.28690004348754883, 0.05058002471923828, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
-        self.notePosition6 = [0.28690004348754883, -0.05058002471923828, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
-        self.notePosition7 = [0.35690004348754883, -0.12458002471923828, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
-        self.notePosition8 = [0.35690004348754883, -0.20958002471923828, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
-        #self.motionProxy.rest()
-        self.motionProxy.setStiffnesses("Body",1.0)
+        self.notePosition1 = [5*almath.TO_RAD, 0.425580539703369, -1.488802108764648, -0.4662940502166748, 1.453660583496094]
+        self.notePosition2 = [5*almath.TO_RAD, 0.3210121536254883, -1.48802108764648, -0.4662940502166748, 1.453660583496094]
+        self.notePosition3 = [5*almath.TO_RAD, 0.2421559715270996, -1.48802108764648, -0.4662940502166748, 1.453660583496094]
+        self.notePosition4 = [5*almath.TO_RAD, 0.1368161201477051, -1.48802108764648, -0.4662940502166748, 1.453660583496094]
+        self.notePosition5 = [5*almath.TO_RAD, -0.2823560428619385, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
+        self.notePosition6 = [5*almath.TO_RAD, -0.36058002471923828, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
+        self.notePosition7 = [5*almath.TO_RAD, -0.458002471923828, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
+        self.notePosition8 = [5*almath.TO_RAD, -0.52958002471923828, 1.483336091041565, 0.357464075088501, -1.3683700561523438]
+        self.song_1 = [1, 1, 2, 1, 4, 3, 1, 1, 2, 1,5,4, 1, 1, 8, 6, 4, 3, 2, 7, 7, 6, 5, 4] #Happy Birthday
+        self.song_2 = [1, 2, 3, 1, 3, 1, 3,2,3,4,4,3,2,4,3,4,5,3,5,3,5,4,5,6,6,5,4,6,]
+        self.motionProxy.rest()
+        # self.motionProxy.setStiffnesses("Body",1.0)
         # time.sleep(2)
         #self.postureProxy.goToPosture("Crouch", 0.5)
-        self.motionProxy.setAngles("LHipYawPitch", -0.190174, self.fractionMaxSpeed)
+        #time.sleep(5)
+        # #self.motionProxy.setAngles(["LHipPitch", "RHipPitch"], [-0.8, -0.8], self.fractionMaxSpeed)
         # self.motionProxy.setStiffnesses("LArm",0.0) #Disable stiffness in the arm
         # self.motionProxy.setStiffnesses("RArm",0.0) #Disable stiffness in the arm
 
-        # # self.motionProxy.setStiffnesses("LArm", 1.0) #Enable stiffness in the arm
-        # self.motionProxy.openHand('LHand')
-        # time.sleep(2)
-        # self.motionProxy.closeHand("LHand")
+        #self.grabStick()
         # self.motionProxy.setStiffnesses("LArm", 1.0) #Enable stiffness in the arm
         # time.sleep(2)   
         
@@ -68,9 +68,15 @@ class notePositions:
 
 
     def grabStick(self):
-        self.motionProxy.openHand('LHand')
+        self.motionProxy.setStiffnesses("RHand", 1.0)
+        self.motionProxy.setStiffnesses("LHand", 1.0)
+        self.motionProxy.setAngles("LHand", 1.0, 1.0)
+        self.motionProxy.setAngles("RHand", 1.0, 1.0)
         time.sleep(2)
-        self.motionProxy.closeHand('LHand')
+        self.motionProxy.setStiffnesses("RHand", 1.0)
+        self.motionProxy.setStiffnesses("LHand", 1.0)
+        self.motionProxy.setAngles("LHand", 0.0, 1.0)
+        self.motionProxy.setAngles("RHand", 0.0, 1.0)
 
         
     def callback_img(self, data):
@@ -95,136 +101,111 @@ class notePositions:
         arm_angles = self.motionProxy.getAngles(names,useSensorValues)
         return arm_angles
 
-    def hitNote(self,chainName,notePosition):
+    def hitNote(self,note):
         """
         Position the arm above the note and turn wrist and shoulder to hit the note
         chainName: LArm or RArm
         notePosition: init position for the note
         """
-        if chainName == "LArm":
+        if note==1:
             names = ["LShoulderPitch","LShoulderRoll","LElbowYaw","LElbowRoll","LWristYaw"]
-        elif chainName == "RArm":
+            notePosition=self.notePosition1
+        elif note == 2:
+            names = ["LShoulderPitch","LShoulderRoll","LElbowYaw","LElbowRoll","LWristYaw"]
+            notePosition=self.notePosition2
+        elif note == 3:
+            names = ["LShoulderPitch","LShoulderRoll","LElbowYaw","LElbowRoll","LWristYaw"]
+            notePosition=self.notePosition3
+        elif note == 4:
+            names = ["LShoulderPitch","LShoulderRoll","LElbowYaw","LElbowRoll","LWristYaw"]
+            notePosition=self.notePosition4
+        elif note == 5:
             names = ["RShoulderPitch","RShoulderRoll","RElbowYaw","RElbowRoll","RWristYaw"]
-        self.motionProxy.angleInterpolationWithSpeed(names, notePosition, self.fractionMaxSpeed)
+            notePosition=self.notePosition5
+        elif note == 6:
+            names = ["RShoulderPitch","RShoulderRoll","RElbowYaw","RElbowRoll","RWristYaw"]
+            notePosition=self.notePosition6
+        elif note == 7:
+            names = ["RShoulderPitch","RShoulderRoll","RElbowYaw","RElbowRoll","RWristYaw"]
+            notePosition=self.notePosition7
+        elif note == 8:
+            names = ["RShoulderPitch","RShoulderRoll","RElbowYaw","RElbowRoll","RWristYaw"]
+            notePosition=self.notePosition8
 
-        axisMaskList = [motion.AXIS_MASK_VEL]
-        timeList     = [[1.0]]         # seconds
+        self.motionProxy.angleInterpolationWithSpeed(names, notePosition, self.fractionMaxSpeed)
 
         ## Move arm to position above note
         # self.motionProxy.angleInterpolation([chainName], frame, notePosition,
         #                             axisMaskList, timeList)
-        self.turnDown(chainName)
-        #self.motionProxy.angleInterpolationWithSpeed(names, notePosition, self.fractionMaxSpeed)
-        time.sleep(1.0)
+
+
+        if note in [1,2,3,4]:
+            joints = ["LElbowYaw", "LShoulderPitch"]
+            elbow_angle=20.0
+            shoulder_angle = 30
+        elif note in [5,6,7,8]:
+            joints = ["RElbowYaw", "RShoulderPitch"]
+            elbow_angle=-20.0
+            shoulder_angle = 30
+        angleLists  = [[elbow_angle*almath.TO_RAD, 0.0], [shoulder_angle*almath.TO_RAD, 0.0]]
+        timeLists   = [[1.0, 2.0], [ 1.0, 2.0]]
+        isAbsolute = False  #angle relative to current position
+        self.motionProxy.angleInterpolation(joints, angleLists, timeLists, isAbsolute)
+
 
 
     def turnDown(self,chainName):
-        wrist_angle=0.0
         names=[]
         if chainName == "LArm":
-            names = ["LWristYaw", "LShoulderPitch"]
-            wrist_angle=20.0
-            shoulder_angle = 20
+            names = ["LElbowYaw", "LShoulderPitch"]
+            elbow_angle=20.0
+            shoulder_angle = 30
         elif chainName == "RArm":
-            names = ["RWristYaw", "RShoulderPitch"]
-            wrist_angle=-20.0
-            shoulder_angle = 20
+            names = ["RElbowYaw", "RShoulderPitch"]
+            elbow_angle=-20.0
+            shoulder_angle = 30
 
-        angleLists  = [[wrist_angle*almath.TO_RAD, 0.0], [shoulder_angle*almath.TO_RAD, 0.0]]
+        angleLists  = [[elbow_angle*almath.TO_RAD, 0.0], [shoulder_angle*almath.TO_RAD, 0.0]]
         timeLists   = [[1.0, 2.0], [ 1.0, 2.0]]
         isAbsolute = False  #angle relative to current position
         self.motionProxy.angleInterpolation(names, angleLists, timeLists, isAbsolute)
     
 
-    def recordTransform(self,chainName): 
-        chainName = "LArm"
-        frame = motion.FRAME_ROBOT
-        useSensorValues = False
-        tf = almath.Transform(self.motionProxy.getTransform(chainName,frame,useSensorValues))
-        return tf
+    def playSong(self,song):
+        for note in song:
+            self.hitNote(note)
+            time.sleep(1)
 
-    # def playNote(self,notePosition,noteTarget):
-    #     #self.motionProxy.closeHand("LHand")State("LArm")
-    #     useSensor = False
-    #     fractionMaxSpeed = 0.7
-    #     axisMask         = 63 # just control position
-    #     print("move arm")
-    #     current = self.recordJointState("LArm")
-    #     print(current)
-    #     self.motionProxy.setPositions(chainName, frame, notePosition, fractionMaxSpeed, axisMask)
-    #     time.sleep(0.5)
-    #     print("Hit note")
-    #     self.motionProxy.setPositions(chainName, frame, noteTarget, fractionMaxSpeed, axisMask)
-    #     current = self.recordJointState("LArm")
-    #     print(current)
-    #     time.sleep(0.5)
-    #     self.motionProxy.setPositions(chainName, frame, notePosition, fractionMaxSpeed, axisMask)
-    #     time.sleep(2)
-
-
-    def playNote1(self,rhythm):
-        """
-        Plays the first note (C)
-        """
-        chainName = "LArm"
-        frame = motion.FRAME_ROBOT
-        # transform_note_1 = [0.85052, -0.520852, 0.073002, 0.150683,
-        #                     0.520812, 0.853411, 0.0210918, 0.232483,
-        #                     -0.0732864, 0.0200814, 0.997109, 0.272251]
-
-        transform_note_1  = [0.949419, -0.313825, -0.010873, 0.165477,
-                            0.314006, 0.949064, 0.0260411, 0.178851,
-                            0.00214686, -0.0281381, 0.999602, 0.286437]
-
-
-        # transform_hit_note1 = [0.807697, -0.513714, 0.289352, 0.135345,
-        #                     0.546821, 0.836205, -0.0418006, 0.234763,
-        #                     -0.220484, 0.191986, 0.95631, 0.244178]
-
-        transform_hit_note1 = [0.900836, -0.32807, 0.284367, 0.162335,
-                                0.335875, 0.941641, 0.0223509, 0.183376,
-                                -0.275105, 0.0753773, 0.958455, 0.232225]
-
-
-        # transform_hit_note1   =[0.896099, -0.226726, 0.381578, 0.163841,
-        #                         0.315852, 0.929728, -0.189322, 0.221143,
-        #                         -0.31184, 0.290173, 0.904741, 0.242694]
-        fractionMaxSpeed = 0.3
-        axisMask         = 63
-
-        self.motionProxy.setTransforms(chainName, frame, transform_note_1, fractionMaxSpeed, axisMask)
-        time.sleep(rhythm)
-        self.motionProxy.setTransforms(chainName, frame, transform_hit_note1, fractionMaxSpeed, axisMask)
-        time.sleep(rhythm)
-        self.motionProxy.setTransforms(chainName, frame, transform_note_1, fractionMaxSpeed, axisMask)
-        time.sleep(rhythm)
 
     def run(self):
-        # self.motionProxy.setStiffnesses("LArm",1.0) #Disable stiffness in the arm
-        # self.motionProxy.setStiffnesses("RArm",1.0) #Disable stiffness in the arm
+        # self.motionProxy.setStiffnesses("LArm",0.0) #Disable stiffness in the arm
+        # self.motionProxy.setStiffnesses("RArm",0.0) #Disable stiffness in the arm
 
         #self.motionProxy.setStiffnesses("LArm", 1.0)
         time.sleep(4)
-        # current = self.recordTransform("LArm")
-        current = self.recordArmAngles("RArm")
-        print("RArm angles note 1")
-        print(current)
+        # right = self.recordArmAngles("RArm")
+        # print("RArm angles note 1")
+        # print(right)
 
-        self.hitNote("LArm",self.notePosition1)
-        time.sleep(2)
-        self.hitNote("LArm",self.notePosition3)
-        time.sleep(2)
-        self.hitNote("LArm",self.notePosition2)
-        time.sleep(2)
-        self.hitNote("LArm",self.notePosition4)
+        left = self.recordArmAngles("LArm")
+        print("LARm angles note 1")
+        print(left)
 
-        self.hitNote("RArm",self.notePosition5)
-        time.sleep(2)
-        self.hitNote("RArm",self.notePosition6)
-        time.sleep(2)
-        self.hitNote("RArm",self.notePosition7)
-        time.sleep(2)
-        self.hitNote("RArm",self.notePosition8)
+        # self.hitNote("LArm",self.notePosition1)
+        # time.sleep(1)
+        # self.hitNote("LArm",self.notePosition2)
+        # time.sleep(1)
+        # self.hitNote("LArm",self.notePosition3)
+        # time.sleep(1)
+        # self.hitNote("LArm",self.notePosition4)
+        # time.sleep(1)
+        # self.hitNote("RArm",self.notePosition5)
+        # time.sleep(1)
+        # self.hitNote("RArm",self.notePosition6)
+        # time.sleep(1)
+        # self.hitNote("RArm",self.notePosition7)
+        # time.sleep(1)
+        # self.hitNote("RArm",self.notePosition8)
 
 
     
